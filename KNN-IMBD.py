883dcd8ef1f6943fecacce8b14d5286e0d17b7e4
0iris0 <121336378+0iris0@users.noreply.github.com>
2024-06-24 20:05:10 +0800

    confusion matrix要改

--- KNN-IMBD.py
+++ KNN-IMBD.py
@@ -4,8 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 dataset = pd.read_csv("imdb_top_1000.csv")
+
+# 常態性檢定
+#機率圖
+stats.probplot(df["Meta_score"], plot=plt)
+plt.title("IMBD專業影評人給分")
+plt.show()
+#檢定法
+stat, p = stats.kstest(df["Meta_score"], "norm")  # 選擇KS檢定法因n>50
+print('Kolmogorov-Smirnov 檢定統計值=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+    print("符合常態分佈")
+else:
+    print("非常態分布")
+#兩種檢定結果皆顯示非常態分布，原先打算使用線性迴歸改採KNN
+
 
 # 資料前處理
 """排除沒有Meta_score的資料,將IMBD分類為好評(>=7分)與差評(<7分)"""
@@ -47,3 +62,15 @@ plt.title("專業影評人評分 vs IMBD評價")
 plt.xlabel("專業影評人評分")
 plt.ylabel("IMBD評價")
 plt.show()
+
+# 結果分析
+"""
+預測影評分數是好評還差評準確度非常高
+"""
+
+
+# 進階探討方向
+"""
+1.觀眾評分是否受到促銷方式、公關票等影響
+2.高票房不等於好片,專業影評人評分標準如有釋出,可以透過評分標準不同特徵子集合針對IMBD分數與票房做預測
+"""
