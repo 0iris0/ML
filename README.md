12cd701768381f0dd7037d00ed7131572e700a0c
0iris0 <smiling2577@gmail.com>
2024-11-27 20:31:09 +0800

    renew

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# 機器學習方法練習
-1.IMBD預測(KNN
+# 內容簡介
 
-2.是否罹患DIABETES預測(logistic
+1.機器學習模型練習
 
-3.預測SALARY(multiplelinear
+2.SQL操作與連線python
 
-4.SQL操作
+3.kaggle_預測製造是否缺陷
+
+4.work_用電池循環次數預測SOH值
--- defect.py
+++ defect.py
@@ -1,23 +1,52 @@
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-# 預測是否為缺陷，並比較邏輯斯回歸與clustering成效
+# 預測是否為缺陷，並比較羅吉斯回歸與clustering成效
 
 # 匯入資料
 data = pd.read_csv("manufacturing_defect_dataset.csv")
 # print(data)
-data_x = data.iloc[:, :-1]
-data_y = data[["DefectStatus"]]
-# print(data_x)
+x = data.iloc[:, :-1]
+y = data[["DefectStatus"]]
+# print(x)
 
-# 資料每欄確認分布狀態、除錯
-print(data.describe())
-print(data.isnull())
+# 資料每欄確認分布狀態、除錯(排除異常值、填補)
+# print(data.info())
+# print(data.describe())
+# print(data.isnull())
+# print(data.head())
+df_describe = pd.DataFrame(data.describe())
+list_x_names = list(data.describe().columns)
+for d in list_x_names:
+    IQR = x[d].iloc[6]-x[d].iloc[4]
+    outlier_max = x[d].iloc[6]+1.5*IQR
+    outlier_min = x[d].iloc[4]-1.5*IQR
+    normal_range = outlier_max-outlier_min
+    for i in range(len(x)):
+        if x[i] not in normal_range:
+            x = x.drop([i])
+print(x)
+
+# 視覺化
+features = [col for col in x.columns]
+plt.figure(figsize=(15, 5))
+rows = len(features)//3
+for i, feature in enumerate(features, 1):
+    plt.subplot(rows, 3, i)
+    plt.scatter(x[feature], data["DefectStatus"], alpha=0.7, label=feature)
+    plt.xlabel(feature)
+    plt.ylabel("DefectStatus")
+    plt.title(f"{feature} vs DefectStatus")
+    plt.grid(True)
+    # plt.show()
 
 # 計算相關性、共線性
 
+# a = data.corr()
+# print(a)
 # 前處理、標準化、正規化
 
 # 建模
