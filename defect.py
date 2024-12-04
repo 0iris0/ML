e77cdd083c91698721b4469477b6709afb0290b3
0iris0 <smiling2577@gmail.com>
2024-12-04 21:27:51 +0800

    revisualize

--- defect.py
+++ defect.py
@@ -10,38 +10,40 @@ from sklearn.linear_model import LogisticRegression
 data = pd.read_csv("manufacturing_defect_dataset.csv")
 # print(data)
 
-# 資料每欄確認分布狀態、除錯(排除異常值、填補)
+# 資料每欄確認分布狀態、除錯、排除異常值、填補...
 # print(data.info())
 # print(data.describe())
 # print(data.isnull())
 # print(data.head())
-list_x_names = list(data.columns)
+data_x = data.iloc[:, :-1]
+list_x_names = list(data_x.columns)
 for d in list_x_names:
-    per25 = np.percentile(data[d], 25)
-    per75 = np.percentile(data[d], 75)
+    per25 = np.percentile(data_x[d], 25)
+    per75 = np.percentile(data_x[d], 75)
     IQR = per75-per25
     outlier_max = per75+1.5*IQR
     outlier_min = per25-1.5*IQR
-    data = data[(data[d] >= outlier_min) & (data[d] <= outlier_max)]
-data = data.reset_index(drop=True)
-# print(data)
+    data_x = data_x[(data_x[d] >= outlier_min) & (data_x[d] <= outlier_max)]
+data_x = data_x.reset_index(drop=True)
+# print(data_x)
 
 
 # 視覺化
-x = data.iloc[:, :-1]
+x = data_x.iloc[:]
 y = data[["DefectStatus"]]
 # print(x)
 features = [col for col in x.columns]
 plt.figure(figsize=(15, 5))
-rows = len(features)//3
+rows = (len(features)+2)//3
 for i, feature in enumerate(features, 1):
     plt.subplot(rows, 3, i)
     plt.scatter(x[feature], data["DefectStatus"], alpha=0.7, label=feature)
     plt.xlabel(feature)
     plt.ylabel("DefectStatus")
-    plt.title(f"{feature} vs DefectStatus")
+    # plt.title(f"{feature} vs DefectStatus")
     plt.grid(True)
-    # plt.show()
+plt.tight_layout()  # 調整佈局
+# plt.show()
 
 # 計算相關性、共線性
 
